a8f9d183b22ae56d1888afb07bb5fc1b416e68cd
br4hm <haehnlein.alexander@gmx.de>
2023-09-14 12:26:21 +0200

    tryCatch-Funktion einfügen, error-catch, loading-anzeige

--- main.js
+++ main.js
@@ -22,12 +22,20 @@ const apiURL = 'https://api.api-ninjas.com/v1/quotes?category=';
 
 const getQuote = async function() {
     
-   
+    try {
+
+    quoteEle.innerText = 'loading . . .';
+    authorEle.innerText = '';
+    btnEle.style.display= "none";
 	const response = await fetch(apiURL, options);
 	const result = await response.json();
     quoteEle.innerText = '" ' + result[0].quote + ' "';
     authorEle.innerText = '― ' + result[0].author;
 
+    btnEle.style.display= "block";
+} catch (error){
+    alert('Something went wrong. Try again.')
+}
 }
 
-addEventListener('click', getQuote);
+btnEle.addEventListener('click', getQuote);
